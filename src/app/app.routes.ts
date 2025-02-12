20944818243c8fd8eb1641821293091026207331
david esteve <david.esteve66@gmail.com>
2025-02-12 20:32:56 +0100

    avec carte movies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {ListOfMoviesComponent} from './views/movie/list-of-movies/list-of-movies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home',component: ListOfMoviesComponent},
+  { path: 'movies',component: ListOfMoviesComponent},
+
+];
