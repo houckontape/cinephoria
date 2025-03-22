b84ab6ebddd7a0379db0fff92d302273cea2d2a4
david esteve <david.esteve66@gmail.com>
2025-03-22 13:18:58 +0100

    mise en service de la vue  movie details

---
+++ src/app/views/admin/add-movies/add-movies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-movies',
+  imports: [],
+  templateUrl: './add-movies.component.html',
+  styleUrl: './add-movies.component.css'
+})
+export class AddMoviesComponent {
+
+}
--- src/app/views/movie/movies-details/movies-details.component.ts
+++ src/app/views/movie/movies-details/movies-details.component.ts
@@ -3,10 +3,13 @@ import {MoviesService} from '../../../core/service/movies.service';
 import {Movies} from '../../../core/model/movies';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 import {EMPTY, of, switchMap} from 'rxjs';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-movies-details',
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './movies-details.component.html',
   styleUrl: './movies-details.component.css'
 })
